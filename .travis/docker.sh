4601e3ea176205d27db80435c2972f31d7346cde
Philip Wellnitz <philipwellnitz@gmx.de>
2020-09-03 23:55:15 +0900

    travis

---
+++ .travis/docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -ex
+
+source /etc/profile.d/devkit-env.sh
+
+cd P-Emerald_2
+make NOSOUND=1
+mv P-Emerald_2.nds P-Emerald_2_NOSOUND.nds
+make DESQUID=1 NOSOUND=1 rebuild
+mv P-Emerald_2.nds P-Emerald_2_NOSOUND_DESQUID.nds
+make DESQUID=1 rebuild
+mv P-Emerald_2.nds P-Emerald_2_DESQUID.nds
+make rebuild
